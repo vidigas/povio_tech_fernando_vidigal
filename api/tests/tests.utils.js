98b1b0187bea172b59609b4ab53eec90e7a6def7
vidigas <vidigas@bitbucket.org>
2019-04-25 17:37:25 -0300

    implemented react snapshot test

--- api/tests/tests.utils.js
+++ api/tests/tests.utils.js
@@ -13,12 +13,9 @@ export const populateTestDB = async () => {
 
   return db.then( async () => {
     const Users = mongoose.model('users');
-
-    console.log('creating users...');
     await Users.deleteMany({});
 
     await Promise.all(data.users.map(user => new Users(user).save()));
-    console.log('creating users...done');
   })
   .catch(e =>{
     console.log('MONGODB: ', 'Failed to connect.');
